3a1c25277502d006feeadaf2e4012cda31c27cd0
Pavel Lapshin <polapshin@miem.hse.ru>
2021-06-08 00:27:10 +0300

    ADDED: all states for FSM; testing samples

--- typescript/src/6.machine.ts
+++ typescript/src/6.machine.ts
@@ -23,7 +23,10 @@
 * */
 
 
-// test example: A = A + 10.3 - B
+// test examples:
+// 1. A = A + 10.3 - B
+// 2. year=10.3 + 10.5-B
+// 3. Bomb3 =B+ 90.3 - C
 
 function finiteStateMachine(token: string, stateFlag: number) : boolean {
     const convertedValue : string = String(stateFlag);
@@ -94,6 +97,147 @@ function finiteStateMachine(token: string, stateFlag: number) : boolean {
             }
             return true;
         case "7":
-
+            if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z')) {
+                stateFlag = 8;
+                return false;
+            }
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 9;
+                return false
+            }
+            return true;
+        case "8":
+            if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z') ||
+                (token >= "0" && token <= "9")) {
+                stateFlag = 8;
+                return false;
+            }
+            if (token === "+" || token === "-") {
+                stateFlag = 12;
+                return false;
+            }
+            return true;
+        case "9":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 9;
+                return false
+            }
+            if (token === ".") {
+                stateFlag = 10;
+                return false;
+            }
+            return true;
+        case "10":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 11;
+                return false
+            }
+            return true;
+        case "11":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 11;
+                return false
+            }
+            if (token === "+" || token === "-") {
+                stateFlag = 12;
+                return false
+            }
+            return true;
+        case "12":
+            if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z')) {
+                stateFlag = 13;
+                return false;
+            }
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 14;
+                return false
+            }
+            return true;
+        case "13":
+            if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z') ||
+                (token >= "0" && token <= "9")) {
+                stateFlag = 13;
+                return false;
+            }
+            if (token === "+" || token === "-") {
+                stateFlag = 17;
+                return false;
+            }
+            return true;
+        case "14":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 14;
+                return false
+            }
+            if (token === ".") {
+                stateFlag = 15;
+                return false;
+            }
+            return true;
+        case "15":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 16;
+                return false
+            }
+            return true;
+        case "16":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 16;
+                return false
+            }
+            if (token === "+" || token === "-") {
+                stateFlag = 17;
+                return false
+            }
+            return true;
+        case "17":
+            if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z')) {
+                stateFlag = 18;
+                return false;
+            }
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 19;
+                return false
+            }
+            return true;
+        case "18":
+            if ((token >= 'a' && token <= 'z') || (token >= 'A' && token <= 'Z') ||
+                (token >= "0" && token <= "9")) {
+                stateFlag = 18;
+                return false;
+            }
+            if (token === "\n") {
+                stateFlag = 22;
+                return false
+            }
+            return true;
+        case "19":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 19;
+                return false
+            }
+            if (token === ".") {
+                stateFlag = 20;
+                return false;
+            }
+            return true;
+        case "20":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 21;
+                return false
+            }
+            return true;
+        case "21":
+            if ((token >= "0" && token <= "9")) {
+                stateFlag = 21;
+                return false
+            }
+            if (token === "\n") {
+                stateFlag = 22;
+                return false
+            }
+            return true;
+        case "22":
+            return true
     }
 }
